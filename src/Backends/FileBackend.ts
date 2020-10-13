736d2c5767ae2ae90b43dea99b42d7916e8d256f
Pedro M. Silva <pemiolsi@hotmail.com>
2020-10-13 14:33:42 +0100

    Fix concurrency handling in FileBacking

--- src/Backends/FileBackend.ts
+++ src/Backends/FileBackend.ts
@@ -5,6 +5,7 @@ import makeDir from 'make-dir';
 import tpl from 'mnml-tpl';
 import { format } from "date-fns";
 import stripAnsi from 'strip-ansi';
+import { Semaphore } from 'data-semaphore';
 
 export class FileBackend implements Backend {
     pattern : string;
@@ -15,10 +16,14 @@ export class FileBackend implements Backend {
 
     stream : fs.WriteStream;
 
+    protected streamChangeSemaphore : Semaphore;
+
     constructor ( pattern : string ) {
         this.pattern = pattern;
 
         this.patternFactory = tpl( this.pattern );
+
+        this.streamChangeSemaphore = new Semaphore( 1 );
     }
 
     protected getLocals () {
@@ -40,15 +45,27 @@ export class FileBackend implements Backend {
     protected writeRaw ( data : object ) : void {
         const file = this.patternFactory( this.getLocals() );
         
-        if ( !this.lastFile || this.lastFile != file ) {
-            if ( this.stream ) {
-                this.stream.close();
-            }
+        if ( this.lastFile != file ) {
+            this.lastFile = null;
+        }
 
-            makeDir( path.dirname( file ) ).then( () => {
-                this.stream = fs.createWriteStream( file, { flags: 'a', encoding: 'utf8' } );
+        if ( this.lastFile == null ) {
+            this.streamChangeSemaphore.use( async () => {
+                // Since we're using a semaphore, calls can be paused in a queue
+                // And once resolved, the value of lastFile (which was != file
+                // when they were initially called) might have changed.
+                // As such we must check inside the semaphore as well
+                if ( this.lastFile != file ) {
+                    if ( this.stream ) {
+                        this.stream.close();
+                    }
+
+                    await makeDir( path.dirname( file ) );
     
-                this.lastFile = file;
+                    this.stream = fs.createWriteStream( file, { flags: 'a', encoding: 'utf8' } );
+        
+                    this.lastFile = file;
+                }
 
                 this.stream.write( JSON.stringify( data ) + '\n' );
             } ).catch( error => console.error( error ) );
